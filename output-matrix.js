0cce0a3e2738f906e3ab056e384e6162ae58d9d2
Neno Almeida <nenoxvx@icloud.com>
2016-09-26 06:34:48 -0300

    Create output-matrix.js

---
+++ output-matrix.js
@@ -0,0 +1,8 @@
+
+for (var i=0; i<matrix3x3x3.length; i++){
+  for (var j=0; j<matrix3x3x3[i].length; j++){
+    for (var z=0; z<matrix3x3x3[i][j].length; z++){
+      console.log(matrix3x3x3[i][j][z]);
+      }
+   }
+}
